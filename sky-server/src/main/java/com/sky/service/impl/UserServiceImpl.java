080be87135bb956bc6ce705732eded0810309b91
lzhgod666 <1225732228@qq.com>
2025-08-08 10:14:39 +0800

    修复小程序用户登录功能

--- sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
@@ -1,10 +1,16 @@
 package com.sky.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.constant.MessageConstant;
 import com.sky.dto.UserLoginDTO;
 import com.sky.entity.User;
+import com.sky.exception.LoginFailedException;
 import com.sky.mapper.UserMapper;
+import com.sky.properties.WeChatProperties;
 import com.sky.service.UserService;
+import com.sky.utils.HttpClientUtil;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.UserLoginVO;
 import com.sky.properties.JwtProperties;
@@ -23,9 +29,24 @@ public class UserServiceImpl implements UserService {
     private UserMapper userMapper;
     @Autowired
     private JwtProperties jwtProperties;
+    @Autowired
+    private WeChatProperties weChatProperties;
+    public static final String WX_LOGIN = "https://api.weixin.qq.com/sns/jscode2session";
 
     @Override
     public UserLoginVO login(UserLoginDTO userLoginDTO) {
+        Map<String, String> map = new HashMap<>();
+        map.put("appid", weChatProperties.getAppid());
+        map.put("secret", weChatProperties.getSecret());
+        map.put("js_code", userLoginDTO.getCode());
+        map.put("grant_type", "authorization_code");
+        String json = HttpClientUtil.doGet(WX_LOGIN, map);
+        JSONObject object = JSON.parseObject(json);
+        String openid = object.getString("openid");
+        if (openid == null) {
+            throw new LoginFailedException(MessageConstant.LOGIN_FAILED);
+        }
+        userLoginDTO.setCode(openid);
         UserLoginVO user = userMapper.select(userLoginDTO);
         if (user != null) {
             //登录，获取token
